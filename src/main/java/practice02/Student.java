45ef75ad6b120f6a846ef5d7c1ac9644dd211d5a
fanyafeng9831 <1090986665@qq.com>
2019-08-23 09:52:40 +0800

    practice02

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,30 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+	
+	private int classNum;
+
+	public Student(String name, int age, int classNum) {
+		super(name, age);
+		this.classNum = classNum;
+	}
+
+
+
+	public int getClassNum() {
+		return classNum;
+	}
+
+
+
+	public void setClassNum(int classNum) {
+		this.classNum = classNum;
+	}
+
+
+
+	@Override
+	public String introduce() {
+		return String.format("I am a Student. I am at Class %d.", this.getClassNum());
+	}
 }
